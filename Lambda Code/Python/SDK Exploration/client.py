59ca963e3060e7a9d7ff835cba0c52bfd8ceac26
Ronald Theodoro <ronaldtheodoro@gmail.com>
2021-04-17 01:22:01 -0300

    Change bucket name

--- Lambda Code/Python/SDK Exploration/client.py
+++ Lambda Code/Python/SDK Exploration/client.py
@@ -13,8 +13,8 @@ import boto3
 
 client = boto3.client('s3')
 
-response = client.list_objects(Bucket='<BUCKET_NAME>')
+response = client.list_objects(Bucket='catch-dragons')
 
 for content in response['Contents']:
-    obj_dict = client.get_object(Bucket='<BUCKET_NAME>', Key=content['Key'])
+    obj_dict = client.get_object(Bucket='catch-dragons', Key=content['Key'])
     print(content['Key'], obj_dict['LastModified'])
